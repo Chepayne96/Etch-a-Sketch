0a15f4a6ebddcd4bf4439d14f030e25d77ef27cf
Chepayne96 <payne_cheyenne@outlook.com>
2024-04-04 21:42:48 -0400

    Got the project done. Choose not to do the extra credit as I wanna move on to the other projects

--- function.js
+++ function.js
@@ -1,15 +1,31 @@
 const sketchBox = document.querySelector('#sketchContainer');
 
+
 // Create a 16 by 16 square grid
-function createGrid() {
-    for (let i = 0; i < 257; i++){
-        const sketchGrid = document.createElement('div');
-        sketchGrid.classList.add('sketch');
-        sketchGrid.addEventListener('mouseover', (event) => {
-            sketchGrid.classList.replace('sketch', 'sketchActive');
-        });
-        sketchBox.appendChild(sketchGrid);
+function createGrid(gridSize) {
+    for (w = 0; w < gridSize; w++) {
+        for (h = 0; h < gridSize; h++) {
+            const sketchGrid = document.createElement('div');
+            sketchGrid.classList.add('sketch');
+            sketchGrid.style.width = 960 / gridSize + 'px';
+            sketchGrid.style.height = 960 / gridSize + 'px';
+            sketchBox.appendChild(sketchGrid);
+            // Change the color of each square when 'mouseover'
+            sketchGrid.addEventListener('mouseover', (event) => {
+                sketchGrid.classList.replace('sketch', 'sketchActive');
+                
+            });
+        }
     }
 }
 
-createGrid();
+
+
+
+
+
+function startGrid() {
+    let size = prompt('Grid size: ');
+    document.getElementById('sketchContainer').innerHTML = '';
+    createGrid(size);
+}
